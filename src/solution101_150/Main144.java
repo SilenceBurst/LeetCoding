bb9ce0a51e15a71dd338bdf2ba08b8cd96add6de
[SilenceBurst] <yongshengdev@163.com>
2020-11-18 12:02:50 +0800

    二叉树的前序遍历

--- src/solution101_150/Main144.java
+++ src/solution101_150/Main144.java
@@ -54,47 +54,6 @@ public class Main144 {
         }
     }
 
-
-//    /**
-//     * 递归解法
-//     */
-//    public static List<Integer> preorderTraversal1(TreeNode root) {
-//        List<Integer> output = new ArrayList<Integer>();
-//        helper1(output, root);
-//        return output;
-//    }
-//
-//    public static void helper1(List<Integer> output, TreeNode node) {
-//        if (node != null) {
-//            output.add(node.val);
-//            helper1(output, node.left);
-//            helper1(output, node.right);
-//        }
-//    }
-//
-//    /**
-//     * 迭代解法
-//     */
-//    public static List<Integer> preorderTraversal2(TreeNode root) {
-//        LinkedList<Integer> output = new LinkedList<>();
-//        if (root == null) {
-//            return output;
-//        }
-//        LinkedList<TreeNode> stack = new LinkedList<>();
-//        stack.add(root);
-//        while (!stack.isEmpty()) {
-//            TreeNode node = stack.pollLast();
-//            output.add(node.val);
-//            if (node.right != null) {
-//                stack.add(node.right);
-//            }
-//            if (node.left != null) {
-//                stack.add(node.left);
-//            }
-//        }
-//        return output;
-//    }
-
     public class TreeNode {
         int val;
         TreeNode left;
