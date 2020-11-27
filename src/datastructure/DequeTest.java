b0c80c6243341fb4462a04636def251b422197fc
[SilenceBurst] <yongshengdev@163.com>
2020-11-27 10:57:50 +0800

    Deque

--- src/datastructure/DequeTest.java
+++ src/datastructure/DequeTest.java
@@ -11,7 +11,7 @@ public class DequeTest {
      * <p> 向队列中插入null值 是不合法的
      *
      * 对比Queue有增加了一套从队尾获取/增加/删除元素的api
-     * 队首                             队尾
+     *                  队首                             队尾
      * 获取元素     peekFirst/getFirst              peekLast/getLast
      * 添加元素     offerFirst/addFirst             offerLast/addLast
      * 删除元素     pollFirst/removeFirst           pollLast/removeLast
