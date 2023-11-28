1ac0ee88fc0b1462ffb5b2561774839de39000df
曹永胜 <caoyongsheng@dailyyoga.com>
2023-11-28 09:57:20 +0800

    组装可复制的代码

--- src/parser/ManifestParser.java
+++ src/parser/ManifestParser.java
@@ -8,6 +8,8 @@ import org.w3c.dom.NodeList;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ManifestParser {
 
@@ -18,21 +20,32 @@ public class ManifestParser {
             DocumentBuilder builder = factory.newDocumentBuilder();
             Document document = builder.parse(manifestFile);
 
+            List<Node> notDeclareList = new ArrayList<>();
+
             NodeList activities = document.getElementsByTagName("activity");
-            printComponentsInfo(activities, "Activity");
+            printComponentsInfo(activities, "Activity", notDeclareList);
 
             NodeList services = document.getElementsByTagName("service");
-            printComponentsInfo(services, "Service");
+            printComponentsInfo(services, "Service", notDeclareList);
 
             NodeList receivers = document.getElementsByTagName("receiver");
-            printComponentsInfo(receivers, "Receiver");
-
+            printComponentsInfo(receivers, "Receiver", notDeclareList);
+            System.out.println("===============printCopyCode==============");
+            printCopyCode(notDeclareList);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static void printComponentsInfo(NodeList nodeList, String componentName) {
+    private static void printCopyCode(List<Node> notDeclareList) {
+        for (Node node : notDeclareList) {
+            Element element = (Element) node;
+            String elementName = element.getAttribute("android:name");
+            System.out.println("<" + node.getNodeName() + " android:name=\"" + elementName + "\" " + "android:exported=\"false\"" + " tools:node=\"merge\"" + " />");
+        }
+    }
+
+    private static void printComponentsInfo(NodeList nodeList, String componentName, List<Node> notDeclareList) {
         for (int i = 0; i < nodeList.getLength(); i++) {
             Node node = nodeList.item(i);
             if (node.getNodeType() == Node.ELEMENT_NODE) {
@@ -41,6 +54,9 @@ public class ManifestParser {
                 String exported = element.getAttribute("android:exported");
                 System.out.println(componentName + " Name: " + name);
                 System.out.println("Exported: " + (exported.isEmpty() ? "Not declared" : exported));
+                if (exported.isEmpty()) {
+                    notDeclareList.add(node);
+                }
             }
         }
     }
